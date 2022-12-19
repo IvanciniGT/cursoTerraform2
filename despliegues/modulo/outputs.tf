c09f1197631272a26fdea8c355e41c64e9049b15
Ubuntu <ubuntu@ip-172-31-44-24.eu-west-1.compute.internal>
2022-12-19 17:48:12 +0000

    modulo ssh

--- despliegues/modulo/outputs.tf
+++ despliegues/modulo/outputs.tf
@@ -1,5 +1,24 @@
 output "keys" {
-    
+    value = 
+            length(tls_private_key.claves) == 0 ? {
+                    privatekey = {
+                                    pem     = file( local.fichero_clave_privada_pem       ) 
+                                    openssh = file( local.fichero_clave_privada_openssh   ) 
+                                 }
+                    publickey  = {
+                                    pem     = file( local.fichero_clave_publica_pem       ) 
+                                    openssh = file( local.fichero_clave_publica_openssh   ) 
+                                 }
+                } : {
+                    privatekey = {
+                                    pem     = tls_private_key.claves[0].private_key_pem
+                                    openssh = tls_private_key.claves[0].private_key_openssh
+                                 }
+                    publickey  = {
+                                    pem     = tls_private_key.claves[0].public_key_pem
+                                    openssh = tls_private_key.claves[0].public_key_openssh
+                                 }
+                }
 }
 
 # keys.publickey.pem
